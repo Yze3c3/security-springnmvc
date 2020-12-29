0154ed68e93c9cbb97de76f0433ff911ba8bf055
Yze3c3 <384018437@qq.com>
2020-12-29 21:58:19 +0800

    security-springmvc

--- src/main/java/com/lsl/security/springmvc/config/ApplicationConfig.java
+++ src/main/java/com/lsl/security/springmvc/config/ApplicationConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Controller;
 
+
+
+
 @Configuration //相当于applicationContext.xml
 @ComponentScan(basePackages = "com.lsl.security.springmvc"
         ,excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION,value = Controller.class)})
